8e4f6244ced9c21b65b480fb5e9d7095983f6a90
sebastian bascuñan <sebastianbascu.vega@gmail.com>
2017-04-05 19:56:07 -0300

    agregue main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var Numero1 = prompt("ingresa un numero")
+var Numero2 = prompt("Ingresa otro numero")
+var resultado = Number(Numero1) + Number(Numero2);
+var resultado2 = Number(Numero1) - Number(Numero2);
+var resultado3 = Number(Numero1) * Number(Numero2);
+var resultado4 = Number(Numero1) / Number(Numero2);
+
+
+alert(" Estos numeros sumados dan " + resultado + " Estos numeros restados dan " + resultado2 + " Estos numeros multiplicados dan " + resultado3 + " Estos numeros divididos dan " + resultado4)
+
+
